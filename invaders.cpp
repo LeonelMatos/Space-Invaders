88228c80bcf164ffc289b31629d7fd9f9c6ac9fa
LeonelMatos <leonellmts@gmail.com>
2025-07-22 23:05:27 +0100

    Updated working game

--- invaders.cpp
+++ invaders.cpp
@@ -1,49 +1,106 @@
 #include "invaders.hpp"
+#include "assets.hpp"
 
 using namespace blit;
 
-///////////////////////////////////////////////////////////////////////////
-//
-// init()
-//
-// setup your game here
-//
+static Surface *invaders_sheet = Surface::load(sprites_invaders);
+
+static const Rect SRC_SHIP   { 0,   0, 11, 8 };
+static const Rect SRC_ALIEN { 11,  0, 11, 8 };
+
+
 void init() {
     set_screen_mode(ScreenMode::hires);
+
+    game.player.pos = { 160 - 8, 210 };
+
+    for (int y = 0; y < 5; y++)
+        for (int x = 0; x < 11; x++)
+            game.invaders[y * 11 + x].pos = { 20 + x *24, 20 + y * 16 };
 }
 
-///////////////////////////////////////////////////////////////////////////
-//
-// render(time)
-//
-// This function is called to perform rendering of the game. time is the 
-// amount if milliseconds elapsed since the start of your game
-//
 void render(uint32_t time) {
-
-    // reset the screen alpha and clipping mask
-    screen.alpha = 255;
-    screen.mask = nullptr;
-
-    // clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
     screen.pen = Pen(0, 0, 0);
     screen.clear();
 
-    // draw a white rectangle
-    screen.pen = Pen(255, 255, 255);
-    screen.rectangle(Rect(0, 0, 320, 14));
 
-    // draw some black text
-    screen.pen = Pen(0, 0, 0);
-    screen.text("Hello PicoSystem!", minimal_font, Point(5, 4));
+    screen.blit(invaders_sheet, SRC_SHIP, game.player.pos);
+
+    for(auto &inv : game.invaders) {
+        if (!inv.alive) continue;
+        
+        screen.blit(invaders_sheet, SRC_ALIEN, inv.pos);
+    }
+
+    screen.pen = Pen(255, 255, 0);
+    for(auto &b : game.bullets)
+        if(b.active)
+            screen.rectangle(Rect(b.pos.x, b.pos.y, 2, 4));
+    
 }
 
-///////////////////////////////////////////////////////////////////////////
-//
-// update(time)
-//
-// This is called to update your game state. time is the 
-// amount if milliseconds elapsed since the start of your game
-//
 void update(uint32_t time) {
+
+    //player movement
+    if (buttons & Button::DPAD_LEFT)
+        game.player.pos.x = std::max(0, game.player.pos.x -2);
+    if (buttons & Button::DPAD_RIGHT)
+        game.player.pos.x = std::min(320-16, game.player.pos.x + 2);
+
+    //fire
+    if ((buttons.pressed & Button::A) && !game.bullets[0].active) {
+        for (auto &b : game.bullets) {
+            if (!b.active) {
+                b.active = true;
+                b.pos = game.player.pos + Point(8, -4);
+                break;
+            }
+        }
+    }
+
+    //move bullets
+    for(auto &b : game.bullets) {
+        if(b.active) {
+            b.pos.y -= 4;
+            if(b.pos.y < 0) b.active = false;
+        }
+    }
+
+    if(time - game.last_move_time > 500) {
+    // check if any edge of the fleet would hit the screen edge
+    bool at_edge = false;
+    for(auto &inv : game.invaders)
+      if(inv.alive && 
+         (inv.pos.x + 8 >= 320 && game.direction == 1 ||
+          inv.pos.x <= 0   && game.direction == -1)) {
+        at_edge = true;
+        break;
+      }
+
+    // shift fleet down + reverse direction if at edge
+    for(auto &inv : game.invaders) {
+      if(inv.alive) {
+        inv.pos.x += game.direction * 8;
+        if(at_edge) inv.pos.y += 8;
+      }
+    }
+
+    if(at_edge) game.direction *= -1;
+    game.last_move_time = time;
+    }
+
+    //collision: bullets at aliens
+    for(auto &b : game.bullets) {
+        if(!b.active) continue;
+        for(auto &inv : game.invaders) {
+            if(inv.alive && Rect(inv.pos, Size(16,8)).contains(b.pos)) {
+                inv.alive = false;
+                b.active = false;
+                break;
+            }
+        }
+    }
+
+
+
 }
--- invaders.hpp
+++ invaders.hpp
@@ -1,1 +1,27 @@
 #include "32blit.hpp"
+#include <array>
+
+using namespace blit;
+
+struct Player {
+  Point   pos;
+  bool    alive{ true };
+};
+
+struct Invader {
+  Point   pos;
+  bool    alive{ true };
+};
+
+struct Bullet {
+  Point   pos;
+  bool    active{ false };
+};
+
+struct Game {
+    Player player;
+    std::array<Invader, 55> invaders;
+    std::array<Bullet, 4> bullets;
+    uint32_t last_move_time{ 0 };
+    int direction{ 1 }; //1=right -1=left
+} game;
